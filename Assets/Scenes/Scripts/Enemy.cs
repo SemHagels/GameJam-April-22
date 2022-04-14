6676343c0cf53bff7b4db8ebd78227c276c24bbe
Tristan <tnooteboom02@student.rocvantwente.nl>
2022-04-14 11:17:23 +0200

    Alles(Game bijna compleet)
    Er moet alleen nog references naar de scenes moet gemaakt en de Prefabs.

---
+++ Assets/Scenes/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {     
+        Destroy(gameObject); 
+    }
+}
