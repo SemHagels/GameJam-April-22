b1fddff88726a2d476e3a3bd2680bdd653a3c9d6
Sipan <akrawie2005@hotmail.com>
2022-04-14 14:28:44 +0200

    Afgemaakt

--- Assets/Scripts/PasswordInputField.cs
+++ Assets/Scripts/PasswordInputField.cs
@@ -7,13 +7,43 @@ public class PasswordInputField : MonoBehaviour
 {
 
     public InputField inputfield;
+    public Text Hint;
+
+   int number = 0;  
+   int Int;
+    public void Start()
+    {
+        number = Random.Range(0, 10);
+        Debug.Log(number);
+    }
 
     public void CheckInput()
     {
-        if (inputfield.text == "password")      // check inputfield contains the string password
+        int.TryParse(inputfield.text, out Int);
+
+        if (Int <= number)
         {
-            Debug.Log("Password accepted");     // just a debug.Log to show that the password is correct (can be removed)
-            SceneManager.LoadScene("Myscene");  // fill in the name of the scene you want to load
+            // Debug.Log("Guess higher");
+            Hint.text = "Guess higher";
         }
+        else if (Int >= number)
+        {
+            //Debug.Log("Guess Lower");
+            Hint.text = "Guess Lower";
+        }
+
+        if (inputfield.text == number.ToString())     // check inputfield contains the string password
+        {
+           //Debug.Log("Password accepted");     // just a debug.Log to show that the password is correct (can be removed)
+            Hint.text = "Password accepted, GG";
+            SceneManager.LoadScene("SampleScene");  // fill in the name of the scene you want to load
+        }
+        else
+        {
+            //Debug.Log("Wrong Password, Please try again");
+          //  Hint.text = "Wrong Password, Please try again";
+        }
+
+        
     }
 }
